ce2616061b963e73490140ae6e8496ab1b5b2f16
Kyle <kylemit@gmail.com>
2014-11-04 02:08:56 -0500

    Added Initial Commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(".bubbler textarea").keyup(function(){
+    var text = $(this).val();
+  
+    var list = text.split(/[^a-zA-Z\d\-']/);
+    var bubbles = "";
+    
+    $.each(list, function (i, word) {
+        var len = word.length;
+        if (len) {
+            var bubble = '<div data-word="' + word +
+                '" data-size="' + len + '"></div>';
+            bubbles += bubble;
+        }
+    });
+  
+    $(this).closest(".bubbler")
+           .find(".output")
+           .html(bubbles);
+  
+}).keyup();
